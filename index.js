3b7c2e73f00fddb8b5eb3d59a69a66dd726d1765
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-02-22 10:52:20 +0300

    feat: add solution for task

---
+++ index.js
@@ -0,0 +1,108 @@
+const undoRedo = (state) => {
+  const actions = [];
+  const undoStack = [];
+
+  const EDIT_VALUE = "edit";
+  const ADD_VALUE = "add";
+  const DELETE_VALUE = "delete";
+
+  const ERROR_MESSAGE = "There is no operation to";
+  const UNDO_MESSAGE = `${ERROR_MESSAGE} undo`;
+  const REDO_MESSAGE = `${ERROR_MESSAGE} redo`;
+
+  const clearUndo = () => {
+    while (undoStack.length) {
+      undoStack.pop();
+    }
+  };
+
+  const undoReducer = (action) => {
+    const { actionType, key } = action;
+    switch (actionType) {
+      case ADD_VALUE: {
+        delete state[key];
+        break;
+      }
+      case EDIT_VALUE: {
+        state[key] = action.prevValue;
+        break;
+      }
+
+      case DELETE_VALUE: {
+        state[key] = action.prevValue;
+        break;
+      }
+    }
+    undoStack.push(action);
+    return state;
+  };
+
+  const redoReducer = (action) => {
+    const { actionType, key } = action;
+    switch (actionType) {
+      case ADD_VALUE: {
+        state[key] = action.currentValue;
+        break;
+      }
+
+      case EDIT_VALUE: {
+        state[key] = action.currentValue;
+        break;
+      }
+
+      case DELETE_VALUE: {
+        delete state[key];
+        break;
+      }
+    }
+    actions.push(action);
+    return state;
+  };
+
+  return {
+    set(key, value) {
+      if (state.hasOwnProperty(key)) {
+        actions.push({
+          actionType: EDIT_VALUE,
+          prevValue: state[key],
+          currentValue: value,
+          key,
+        });
+      } else {
+        actions.push({
+          actionType: ADD_VALUE,
+          currentValue: value,
+          key,
+        });
+      }
+      state[key] = value;
+      clearUndo();
+    },
+    get(key) {
+      return state[key];
+    },
+    del(key) {
+      actions.push({
+        actionType: DELETE_VALUE,
+        prevValue: state[key],
+        key,
+      });
+      delete state[key];
+      clearUndo();
+    },
+
+    undo() {
+      if (!actions.length) {
+        throw new Error(UNDO_MESSAGE);
+      }
+      return undoReducer(actions.pop());
+    },
+
+    redo() {
+      if (!undoStack.length) {
+        throw new Error(REDO_MESSAGE);
+      }
+      return redoReducer(undoStack.pop());
+    },
+  };
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+It is my solution for Undo/Redo Codewars Kata
+
+For a full description, please follow the link:
+
+https://www.codewars.com/kata/531489f2bb244a5b9f00077e
+
+Thanx for your attention and good luck!
